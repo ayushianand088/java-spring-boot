5c54b26650daa1382b48b65b4ce7b4d21e672f40
AyushiAnand70047 <ayushianand088@gmail.com>
2025-10-07 10:11:06 +0530

    Primitive Data Types

---
+++ java/Variables/PrimitiveDataType.java
@@ -0,0 +1,40 @@
+public class PrimitiveDataType {
+    public static void main(String[] args){
+        // char
+        char var1 = 'a';
+        char var2 = 97;
+        System.out.println("Character: " + var1);
+        System.out.println("Character: " + var2);
+
+        // byte
+        byte num1 = 3;
+        byte num2 = -3;
+        System.out.println("Byte: " + num1);
+        System.out.println("Byte: " + num2);
+
+        // short 
+        short num3 = 128;
+        System.out.println("Short: " + num3);
+
+        // int
+        int num4 = 32768;
+        System.out.println("Integer: " + num4);
+
+        // long
+        long num5 = 100l;
+        System.out.println("Long: " + num5);
+
+        // float
+        float num6 = 63.20f;
+        System.out.println("Float: " + num6);
+
+        // double
+        double num7 = 63.20d;
+        System.out.println("Double: " + num7);
+
+        // boolean
+        boolean isUpSkilling = true;
+        System.out.println("Boolean: " + is);
+
+    }
+}
